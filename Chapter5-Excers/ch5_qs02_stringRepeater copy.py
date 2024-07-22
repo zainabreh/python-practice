de24b4f0ba5d147684ba5b1f0424ece1be1400d7
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-07-22 10:50:15 +0500

    Update ch5_qs02_stringRepeater copy.py

--- Chapter5-Excers/ch5_qs02_stringRepeater copy.py
+++ Chapter5-Excers/ch5_qs02_stringRepeater copy.py
@@ -2,7 +2,7 @@ def main():
     str = input("Enter String")
     num = int(input("Enter Number,you want to repeat the string"))
     res = repeat(str,num)
-    print("your Resulted output is:\n",res,"\n")
+    print("your Resulted output is:",res,"")
     
 def repeat(str,n):
     return str * n
